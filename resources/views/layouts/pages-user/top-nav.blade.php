6b9271ecf97cdf91b677f4918f21164d49b591ce
SyarifHidayatullahpens <dsyarif790@gmail.com>
2021-11-11 14:56:26 +0700

    add template login

--- resources/views/layouts/pages-user/top-nav.blade.php
+++ resources/views/layouts/pages-user/top-nav.blade.php
@@ -31,7 +31,7 @@
                 </li>
 
                 <li class="nav-item">
-                    <a class="nav-link" href="#login">Login</a>
+                    <a class="nav-link" href="{{ route('login') }}">Login</a>
                 </li>
             </ul>
         </div>
