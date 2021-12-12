74bdab5643f0c903169af37d117692438086d4ad
AndyYulianto11 <andyyulianto40@github.com>
2021-12-12 16:21:11 +0700

    change image landing page

--- resources/views/layouts/landing-page/top-nav.blade.php
+++ resources/views/layouts/landing-page/top-nav.blade.php
@@ -1,13 +1,13 @@
 <!-- ======= Header ======= -->
 <header id="header" class="d-flex align-items-center">
-    <div class="container d-flex align-items-center justify-content-between">
+    <div class="container d-flex align-items-center line-height">
     <img src="images\pens.png" width="50" height="50" class="logo">&nbsp</img>
       <h1 class="logo"><a href=""><span>PENS</span>&nbspPSDKU&nbspSUMENEP</a></h1>
       <nav id="navbar" class="navbar">
         <ul>
           <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
           <li><a class="nav-link scrollto" href="#about">Tentang Kampus</a></li>
-          <li class="dropdown"><a href="#"><span>Diploma</span> <i class="bi bi-chevron-down"></i></a>
+          <li class="dropdown"><a href="#hero"><span>Diploma</span> <i class="bi bi-chevron-down"></i></a>
           <ul>
           <li><a href="#diploma-ti">Teknik Informatika</a></li>
           <li><a href="#diploma-mmb">Multimedia Broadcasting</a></li>
@@ -15,7 +15,7 @@
               <li><a class="nav-link scrollto" href="#diploma-mmb">Multimedia Kreatif</a></li> -->
           </ul>
           </li>
-          <li class="dropdown"><a href="#"><span>Departements</span> <i class="bi bi-chevron-down"></i></a>
+          <li class="dropdown"><a href="#hero"><span>Departements</span> <i class="bi bi-chevron-down"></i></a>
           <ul>
           <li><a href="#departement-ti">Teknik Informatika</a></li>
           <li><a href="#departement-mmb">Multimedia Broadcasting</a></li>
@@ -23,7 +23,7 @@
               <li><a class="nav-link scrollto" href="departement-mmb">Multimedia Kreatif</a></li> -->
             </ul>
           </li>
-          <li class="dropdown"><a href="#"><span>PMB</span> <i class="bi bi-chevron-down"></i></a>
+          <li class="dropdown"><a href="#hero"><span>PMB</span> <i class="bi bi-chevron-down"></i></a>
           <ul>
           <li><a href="#snmpn">SNMPN</a></li>
           <li><a href="#snmptn">SNMPTN</a></li>
