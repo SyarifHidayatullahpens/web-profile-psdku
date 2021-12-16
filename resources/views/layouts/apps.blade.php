ea48b0310077de5b6ec53e688eb625372a1756f8
SyarifHidayatullah15 <dsyarif790@gmail.com>
2021-12-16 11:54:21 +0700

    Delete asset apps file resource template admin

--- resources/views/layouts/apps.blade.php
+++ resources/views/layouts/apps.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="Start your development with a Dashboard for Bootstrap 4.">
     <meta name="author" content="Creative Tim">
-    <title>PSDKU SUMENEP</title>
+    <title>PENS PSDKU</title>
     <!-- Favicon -->
     <link rel="icon" href="{{ asset('images/pens.png') }}" type="image/png">
 
@@ -55,11 +55,8 @@
     <link rel="stylesheet" href="{{ asset('vendor/nucleo/css/nucleo.css')}}" type="text/css">
     <link rel="stylesheet" href="{{ asset('css/loader.css')}}" type="text/css">
     <link rel="stylesheet" href="{{ asset('vendor/@fortawesome/fontawesome-free/css/all.min.css')}}" type="text/css">
-    <link rel="stylesheet" href="{{asset('vendor/select2/dist/css/select2.min.css')}}">
     
-    <script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
-    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
     
     <!-- Argon CSS -->
     <link rel="stylesheet" href="{{ asset('css/argon.css?v=1.1.0')}}" type="text/css">
@@ -77,6 +74,9 @@
     @include('layouts.toastr')
     <!-- Core -->
     @notifyJs
+    <!-- Select2 -->
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+    
     <script src="toastr.js"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"
         integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
